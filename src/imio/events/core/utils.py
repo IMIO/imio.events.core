44150cda3b74d450bd1a487ee253e7c052c6277d
Christophe Boulanger <christophe.boulanger@imio.be>
2021-02-11 14:43:24 +0100

    remove useless method.

--- src/imio/events/core/utils.py
+++ src/imio/events/core/utils.py
@@ -7,19 +7,6 @@ from plone import api
 import os
 
 
-def create_faceted_folder(container, title, id=None):
-    if id:
-        folder = api.content.create(
-            type="Folder", title=title, container=container, id=id
-        )
-    else:
-        folder = api.content.create(type="Folder", title=title, container=container)
-    api.content.transition(folder, to_state="published")
-    subtyper = folder.restrictedTraverse("@@faceted_subtyper")
-    subtyper.enable()
-    return folder
-
-
 class SortedTaxonomyImportAdapter(TaxonomyImportExportAdapter):
     def importDocument(self, taxonomy, document, clear=False):
         tree = fromstring(document)
