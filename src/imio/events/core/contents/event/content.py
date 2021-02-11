d377478308871dc0ab0947cb6b313e9492d2cc67
Laurent Lasudry <lasudry@gmail.com>
2021-02-11 12:18:57 +0100

    Cleanup

--- src/imio/events/core/contents/event/content.py
+++ src/imio/events/core/contents/event/content.py
@@ -1,18 +1,8 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+
 from plone.dexterity.content import Container
-
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
 from zope.interface import implementer
-
-
-# from imio.events.core import _
 
 
 class IEvent(model.Schema):
