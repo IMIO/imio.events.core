74c64ec71060c54dbbf37293619f127df8f15049
Christophe Boulanger <christophe.boulanger@imio.be>
2021-08-12 15:59:34 +0200

    Fix entity import

--- src/imio/events/core/contents/__init__.py
+++ src/imio/events/core/contents/__init__.py
@@ -1,4 +1,5 @@
 from .agenda.content import IAgenda, Agenda
+from .entity.content import IEntity, Entity
 from .event.content import IEvent, Event
 from .folder.content import IFolder, Folder
 from .page.content import IPage, Page
