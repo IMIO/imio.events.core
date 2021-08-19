48b02f445060a973073c5b89852eb9dee841c210
Christophe Boulanger <christophe.boulanger@imio.be>
2021-08-19 10:11:51 +0200

    Add subscribers to configure faceted after entity and agenda creation

---
+++ src/imio/events/core/subscribers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.common.faceted.utils import configure_faceted
+import os
+
+
+def added_agenda(obj, event):
+    faceted_config_path = "{}/faceted/config/events.xml".format(
+        os.path.dirname(__file__)
+    )
+    configure_faceted(obj, faceted_config_path)
+
+def added_entity(obj, event):
+    added_agenda(obj, event)
