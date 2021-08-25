056d5dc21ec1c13bcffbb15a74a160a64e9a530a
Christophe Boulanger <christophe.boulanger@imio.be>
2021-08-25 13:06:16 +0200

    Update vocabularies.py
    Change & to "and" to avoid widget bug

--- src/imio/events/core/vocabularies.py
+++ src/imio/events/core/vocabularies.py
@@ -11,16 +11,16 @@ from zope.schema.vocabulary import SimpleVocabulary
 class EventsCategoriesVocabularyFactory:
     def __call__(self, context=None):
         values = [
-            (u"stroll_discovery", _(u"Stroll & discovery")),
-            (u"flea_market_market", _(u"Flea market & market")),
-            (u"concert_festival", _(u"Concert & festival")),
-            (u"conference_debate", _(u"Conference & debate")),
+            (u"stroll_discovery", _(u"Stroll and discovery")),
+            (u"flea_market_market", _(u"Flea market and market")),
+            (u"concert_festival", _(u"Concert and festival")),
+            (u"conference_debate", _(u"Conference and debate")),
             (u"exhibit", _(u"Exhibit")),
             (u"movie", _(u"Movie")),
-            (u"party_folklore", _(u"Party & folklore")),
-            (u"trade_fair_fair", _(u"Trade Fair & Fair")),
-            (u"internships_courses", _(u"Internships & courses")),
-            (u"theater_show", _(u"Theater & show")),
+            (u"party_folklore", _(u"Party and folklore")),
+            (u"trade_fair_fair", _(u"Trade Fair and Fair")),
+            (u"internships_courses", _(u"Internships and courses")),
+            (u"theater_show", _(u"Theater and show")),
         ]
         terms = [SimpleTerm(value=t[0], token=t[0], title=t[1]) for t in values]
         return SimpleVocabulary(terms)
