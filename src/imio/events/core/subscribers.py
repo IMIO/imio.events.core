a3ecfa9709f5a738d156700a3e5f170823a9b286
Christophe Boulanger <christophe.boulanger@imio.be>
2021-09-01 15:45:11 +0200

    Add subscribers to add current agenda into event selected_agendas field.

--- src/imio/events/core/subscribers.py
+++ src/imio/events/core/subscribers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from imio.events.core.utils import get_agenda_uid_for_event
 from imio.smartweb.common.faceted.utils import configure_faceted
 import os
 
@@ -13,3 +14,15 @@ def added_agenda(obj, event):
 
 def added_entity(obj, event):
     added_agenda(obj, event)
+
+
+def modified_event(obj, event):
+    uid = get_agenda_uid_for_event(obj)
+    if uid in obj.selected_agendas:
+        return
+    obj.selected_agendas = obj.selected_agendas + [uid]
+    obj.reindexObject()
+
+
+def added_event(obj, event):
+    modified_event(obj, event)
---
+++ src/imio/events/core/utils.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from imio.events.core.contents import IAgenda
+
+
+def get_agenda_uid_for_event(event):
+    obj = event
+    while not IAgenda.providedBy(obj):
+        parent = aq_parent(aq_inner(obj))
+        obj = parent
+    agenda = obj
+    return agenda.UID()
