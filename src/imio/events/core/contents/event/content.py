bc77fcf7992e65c9ac4206ecb87b3bf488a63945
Laurent Lasudry <lasudry@gmail.com>
2021-09-21 16:16:19 +0200

    Move event_type field, change widget & default

--- src/imio/events/core/contents/event/content.py
+++ src/imio/events/core/contents/event/content.py
@@ -8,6 +8,7 @@ from plone.autoform.directives import read_permission
 from plone.autoform.directives import write_permission
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
 from zope.interface import implementer
 from zope.interface import provider
@@ -53,6 +54,15 @@ def get_current_agenda_UID(context):
 
 class IEvent(IAddress):
     """Marker interface and Dexterity Python Schema for Event"""
+
+    directives.order_before(event_type="IBasic.title")
+    directives.widget(event_type=RadioFieldWidget)
+    event_type = schema.Choice(
+        title=_(u"Event type"),
+        source="imio.events.vocabulary.EventTypes",
+        default="event-driven",
+        required=True,
+    )
 
     video_url = schema.URI(
         title=_(u"Video url"),
@@ -133,12 +143,6 @@ class IEvent(IAddress):
             u"Important! These categories allow you to use criteria that are specific to your organization"
         ),
         source="imio.events.vocabulary.EventsLocalCategories",
-        required=False,
-    )
-
-    event_type = schema.Choice(
-        title=_(u"Event type"),
-        source="imio.events.vocabulary.EventTypes",
         required=False,
     )
 
