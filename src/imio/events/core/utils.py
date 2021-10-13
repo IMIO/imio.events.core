48d7caebd89463da97f913511252fc9d54bc0774
Laurent Lasudry <lasudry@gmail.com>
2021-10-13 10:00:27 +0200

    Simplify code

--- src/imio/events/core/utils.py
+++ src/imio/events/core/utils.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from Acquisition import aq_inner
-from Acquisition import aq_parent
 from imio.events.core.contents import IAgenda
+from Products.CMFPlone.utils import parent
 
 
 def get_agenda_for_event(event):
     obj = event
     while not IAgenda.providedBy(obj):
-        parent = aq_parent(aq_inner(obj))
-        obj = parent
+        obj = parent(obj)
     agenda = obj
     return agenda
