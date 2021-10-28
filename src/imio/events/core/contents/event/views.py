55f6606ad087647536f9f8d714db0abefe6c5003
Christophe Boulanger <christophe.boulanger@imio.be>
2021-10-28 13:24:47 +0200

    Display all event fields

--- src/imio/events/core/contents/event/views.py
+++ src/imio/events/core/contents/event/views.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from embeddify import Embedder
+from imio.smartweb.common.utils import translate_vocabulary_term
+from imio.smartweb.locales import SmartwebMessageFactory as _
 from plone.app.contenttypes.behaviors.leadimage import ILeadImage
 from plone.app.contenttypes.browser.folder import FolderView
 from plone.app.event.browser.event_view import EventView
+from zope.i18n import translate
 
 
 class View(EventView, FolderView):
@@ -26,3 +29,29 @@ class View(EventView, FolderView):
     def get_embed_video(self):
         embedder = Embedder(width=800, height=600)
         return embedder(self.context.video_url, params=dict(autoplay=False))
+
+    def category(self):
+        term = translate_vocabulary_term(
+            "imio.events.vocabulary.EventsCategories", self.context.category
+        )
+        if term is None:
+            return
+        return term.title()
+
+    def topics(self):
+        topics = self.context.topics
+        items = []
+        for item in topics:
+            term = translate_vocabulary_term("imio.smartweb.vocabulary.Topics", item)
+            translated_title = translate(_(term), context=self.request)
+            items.append(translated_title)
+        return ", ".join(items)
+
+    def iam(self):
+        iam = self.context.iam
+        items = []
+        for item in iam:
+            term = translate_vocabulary_term("imio.smartweb.vocabulary.IAm", item)
+            translated_title = translate(_(term), context=self.request)
+            items.append(translated_title)
+        return ", ".join(items)
