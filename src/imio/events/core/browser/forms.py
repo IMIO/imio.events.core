0a17f897a350d1cf6520502f036881fb1c91ad32
thomlamb <lambertthomas10@gmail.com>
2026-06-08 13:58:15 +0200

    Move address fieldset above geolocation and translate groups last in event edit form

--- src/imio/events/core/browser/forms.py
+++ src/imio/events/core/browser/forms.py
@@ -11,11 +11,53 @@ from plone.z3cform import layout
 from Products.statusmessages.interfaces import IStatusMessage
 from zope.interface import Invalid
 from z3c.form import button
+from z3c.form.field import Fields
+from z3c.form.group import Group
 from z3c.form.interfaces import WidgetActionExecutionError
 from zope.event import notify
 
+GEO_FIELD_KEY = "IGeolocatable.geolocation"
+
+
+TRANSLATION_GROUPS = {"de_translations", "nl_translations", "en_translations"}
+
+
+def _update_event_form_fields(form):
+    groups = list(form.groups)
+
+    for group in groups:
+        if group.__name__ == "address":
+            group.label = _("Event location")
+            break
+
+    if GEO_FIELD_KEY in form.fields:
+        geo_field = form.fields[GEO_FIELD_KEY]
+        form.fields = form.fields.omit(GEO_FIELD_KEY)
+
+        geo_group = Group(form.context, form.request, form)
+        geo_group.__name__ = "geolocation"
+        geo_group.label = ""
+        geo_group.fields = Fields(geo_field)
+
+        address_pos = next(
+            (i for i, g in enumerate(groups) if g.__name__ == "address"), None
+        )
+        if address_pos is not None:
+            groups.insert(address_pos + 1, geo_group)
+        else:
+            groups.append(geo_group)
+
+    # Move translation groups to the end
+    non_translation = [g for g in groups if g.__name__ not in TRANSLATION_GROUPS]
+    translation = [g for g in groups if g.__name__ in TRANSLATION_GROUPS]
+    form.groups = tuple(non_translation + translation)
+
 
 class EventCustomEditForm(IACategorizeEditForm):
+    def updateFields(self):
+        super().updateFields()
+        _update_event_form_fields(self)
+
     @button.buttonAndHandler(DMF_("Save"), name="save")
     def handleApply(self, action):
         data, errors = self.extractData()
@@ -41,6 +83,10 @@ EventCustomEditView = layout.wrap_form(EventCustomEditForm)
 
 class EventCustomAddForm(IACategorizeAddForm):
     portal_type = "imio.events.Event"
+
+    def updateFields(self):
+        super().updateFields()
+        _update_event_form_fields(self)
 
     @button.buttonAndHandler(DMF_("Save"), name="save")
     def handleAdd(self, action):
